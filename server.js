03988766f805c39dae198874f91146fbc2be01d9
10tinsinh <cuongdola12081994@gmail.com>
2022-09-27 23:46:30 +0700

    Hello Word!

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-hello word
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/user', (req, res) => {
+    res.send('Hello Cuong!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
